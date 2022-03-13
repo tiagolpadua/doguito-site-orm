ceda73c91e9e43284cfcfabf8a7b6c06073d6790
Tiago <tiagolpadua@gmail.com>
2022-03-13 10:25:14 -0300

    commit inicial

---
+++ public/service/cliente-service.js
@@ -0,0 +1,80 @@
+const BASE_PATH = '/clientes'
+
+const listaClientes = () => {
+    return fetch(`${BASE_PATH}`)
+        .then(resposta => {
+            if (resposta.ok) {
+                return resposta.json()
+            }
+            throw new Error('Não foi possível listar os clientes')
+        })
+}
+
+const criaCliente = (nome, email) => {
+    return fetch(`${BASE_PATH}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email
+        })
+    })
+        .then(resposta => {
+            if (resposta.ok) {
+                return resposta.body
+            }
+            throw new Error('Não foi possível criar um cliente')
+        })
+}
+
+const removeCliente = (id) => {
+    return fetch(`${BASE_PATH}/${id}`, {
+        method: 'DELETE'
+    })
+        .then(resposta => {
+            if (!resposta.ok) {
+                throw new Error('Não foi possível deletar um cliente')
+            }
+        })
+}
+
+const detalhaCliente = (id) => {
+    return fetch(`${BASE_PATH}/${id}`)
+        .then(resposta => {
+            if (resposta.ok) {
+                return resposta.json()
+            }
+
+            throw new Error('Não foi possível detalhar um cliente')
+        })
+}
+
+const atualizaCliente = (id, nome, email) => {
+    return fetch(`${BASE_PATH}/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email
+        })
+    })
+        .then(resposta => {
+            if (resposta.ok) {
+                return resposta.json()
+            }
+            throw new Error('Não foi possível detalhar um cliente')
+        })
+}
+
+export const clienteService = {
+    listaClientes,
+    criaCliente,
+    removeCliente,
+    detalhaCliente,
+    atualizaCliente
+
+}
